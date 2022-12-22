47eefc32108914ba9e71ffb1d5edd5837cae033c
Seidu Mohammed <seidumohammed64@gmail.com>
2022-12-22 03:07:45 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,9 +6,7 @@
 class Square:
     """Represents a square"""
 
-    def __init__(self, size):
+    def __init__(self):
         """Initializing this square class
         Args: size - represnets the size of the square defined
         """
-
-        self.__size = size
