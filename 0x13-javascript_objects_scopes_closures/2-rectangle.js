bdeca4433ed96ff49b22f3e67ad834f038248846
Seidu Mohammed <seidumohammed64@gmail.com>
2023-03-15 05:54:09 +0000

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Check the params provided
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
