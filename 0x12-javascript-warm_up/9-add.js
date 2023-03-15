5558313619cc4b5a27bc26e91af15d9d8cce6fdf
Seidu Mohammed <seidumohammed64@gmail.com>
2023-03-15 22:24:20 +0000

    task 9,10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 const a = process.argv[2];
 const b = process.argv[3];
 
-function add(a, b){
+function add (a, b) {
   return a + b;
 }
 
-console.log(add(parseInt(a),parseInt(b)));
+console.log(add(parseInt(a), parseInt(b)));
