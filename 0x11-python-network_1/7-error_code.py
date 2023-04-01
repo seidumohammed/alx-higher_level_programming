d2a3ae83bc30394c48f776dd554aa43233665b50
Seidu Mohammed <seidumohammed64@gmail.com>
2023-04-01 05:39:49 +0000

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Sends a request to a  URL and displays the response body"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
