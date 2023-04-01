a74151a2af541944c735065367493b31710f56b3
Seidu Mohammed <seidumohammed64@gmail.com>
2023-04-01 05:51:01 +0000

    tasks fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This is a Python script that sends a POST request to a local server 
-at "http://0.0.0.0:5000/search_user" with a query parameter "q" that contains 
+"""This is a Python script that sends a POST request to a local server
+at "http://0.0.0.0:5000/search_user" with a query parameter "q" that contains
 either an empty string or the letter provided as a command-line argument.
 """
 import sys
