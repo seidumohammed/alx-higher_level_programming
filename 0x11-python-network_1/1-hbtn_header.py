3126f6af95809e17eb8c6cf9e065503ce7a1517f
Seidu Mohammed <seidumohammed64@gmail.com>
2023-04-01 04:52:02 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
-        print(dict(res.headers).get('X-Request-Id'))
+        print(dict(res.headers).get("X-Request-Id"))
