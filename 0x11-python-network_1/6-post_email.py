f622f908b0351e37adf73c4dc307576936eb8230
Seidu Mohammed <seidumohammed64@gmail.com>
2023-04-01 05:18:10 +0000

    task 1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a POST request to a URL with an email as arg"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    req = requests.post(url, data=value)
+    print(req.text)
