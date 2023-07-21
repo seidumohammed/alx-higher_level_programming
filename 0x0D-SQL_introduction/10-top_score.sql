0a3d578c222c1d78cdf8815664113ed477abb6ee
Seidu Mohammed <seidumohammed64@gmail.com>
2023-07-21 05:17:34 +0000

    task10 -task 103

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- write a script that lists all records of the table second_table of the db hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- The db name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table ORDER BY score DESC;
